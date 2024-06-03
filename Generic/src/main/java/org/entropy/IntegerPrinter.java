8552a1efdb7b01679a0f25f5d9df59a54d6e3cab
s-chance <entropy.tree@outlook.com>
2024-06-03 22:22:12 +0800

    feat(*): create IntegerPrint class to print the data with Integer type

---
+++ Generic/src/main/java/org/entropy/IntegerPrinter.java
@@ -0,0 +1,13 @@
+package org.entropy;
+
+public class IntegerPrinter {
+    Integer content;
+
+    IntegerPrinter(Integer content) {
+        this.content = content;
+    }
+
+    public void print() {
+        System.out.println(content);
+    }
+}
---
+++ Generic/src/main/java/org/entropy/Main.java
@@ -0,0 +1,8 @@
+package org.entropy;
+
+public class Main {
+    public static void main(String[] args) {
+        IntegerPrinter printer = new IntegerPrinter(123);
+        printer.print();
+    }
+}
