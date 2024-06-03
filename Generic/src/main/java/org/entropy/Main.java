1d0f21f42cc2adf8ad69ef395fb9d3d02ebd0213
s-chance <entropy.tree@outlook.com>
2024-06-03 23:18:33 +0800

    feat(*): the Type-unsafe problem

--- Generic/src/main/java/org/entropy/Main.java
+++ Generic/src/main/java/org/entropy/Main.java
@@ -1,9 +1,23 @@
 package org.entropy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         GenericPrinter<Car> printer = new GenericPrinter<>(new Car());
 //        GenericPrinter<Bus> printer = new GenericPrinter<>(new Bus());
         printer.print();
+
+        // type-unsafe problem
+        // don't use 'Object' as a Generic Type
+        List<Object> list = new ArrayList<>();
+        list.add("123");
+        list.add(456);
+
+        // this code will throw ClassCastException exception when running
+        // String item = (String) list.get(1);
+
+        System.out.println(list);
     }
 }
