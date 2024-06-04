4a830e7d6d7d894d415520fdd857097cc5edc9bd
s-chance <entropy.tree@outlook.com>
2024-06-04 21:25:24 +0800

    feat(*): orElse,orElseGet,orElseThrow method

--- Optional/src/main/java/org/entropy/Main.java
+++ Optional/src/main/java/org/entropy/Main.java
@@ -9,12 +9,24 @@ public class Main {
 
         // why is this code doing the same thing again?
         // that's not what it was meant to be, and it shouldn't be used that way
-        if (optionalUser.isPresent()) {
-            System.out.println(optionalUser.get().getFullName());
-        } else {
-            User defaultUser = new User("default", "default user");
-            System.out.println(defaultUser.getFullName());
-        }
+//        if (optionalUser.isPresent()) {
+//            System.out.println(optionalUser.get().getFullName());
+//        } else {
+//            User defaultUser = new User("default", "default user");
+//            System.out.println(defaultUser.getFullName());
+//        }
+
+        // User will instantiate whether they are null or not
+        User user = optionalUser.orElse(new User("default", "default user"));
+        System.out.println(user.getFullName());
+
+        // User will instantiate only when the optionalUser is null
+        User user2 = optionalUser.orElseGet(() -> new User("default", "default user"));
+        System.out.println(user2.getFullName());
+
+        // throws a custom exception
+//        optionalUser.orElseThrow(() -> new RuntimeException("User not found"));
+
         // if you need to process a large number of null types, that will be a tedious work
 //        if (user != null) {
 //            System.out.println(user.getFullName());
