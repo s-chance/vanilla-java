1ce67530618955f8a1a572ebcf22b2c2857aa6a7
s-chance <entropy.tree@outlook.com>
2024-06-05 22:26:54 +0800

    feat(*): get Class Object by .class

--- Reflection/src/main/java/org/entropy/Main.java
+++ Reflection/src/main/java/org/entropy/Main.java
@@ -2,12 +2,6 @@ package org.entropy;
 
 public class Main {
     public static void main(String[] args) {
-        int field = User.publicStaticField;
-        System.out.println(field);
-        User.myPublicStaticMethod();
-
-        User user = new User("entropy", 31);
-        System.out.println(user.name);
-        user.myPublicMethod();
+        Class<User> userClass = User.class;
     }
 }
