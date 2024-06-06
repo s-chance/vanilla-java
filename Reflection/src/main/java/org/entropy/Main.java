0aa9a57fe5362a8f18e8c0188ba9d63857e05dc3
s-chance <entropy.tree@outlook.com>
2024-06-06 21:00:08 +0800

    feat(*): get constructor and instance

--- Reflection/src/main/java/org/entropy/Main.java
+++ Reflection/src/main/java/org/entropy/Main.java
@@ -1,14 +1,17 @@
 package org.entropy;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class Main {
-    public static void main(String[] args) throws ClassNotFoundException, NoSuchFieldException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchFieldException, IllegalAccessException, NoSuchMethodException, InvocationTargetException, InstantiationException {
         Class<?> clazz = Class.forName("org.entropy.User");
-        Method method = clazz.getDeclaredMethod("myPrivateStaticMethod", String.class);
-        method.setAccessible(true);
-        method.invoke(null, "hello world");
+        Constructor<?> constructor = clazz.getDeclaredConstructor(String.class, int.class);
+        Object obj = constructor.newInstance("entropy", 55);
+        if (obj instanceof User) {
+            User user = (User) obj;
+        }
     }
 }
