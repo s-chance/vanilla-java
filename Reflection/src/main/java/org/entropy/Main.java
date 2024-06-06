5ba1ae90df316c65965e9bb332bb2c79817191bc
s-chance <entropy.tree@outlook.com>
2024-06-06 20:29:11 +0800

    feat(*): invoke methods

--- Reflection/src/main/java/org/entropy/Main.java
+++ Reflection/src/main/java/org/entropy/Main.java
@@ -1,14 +1,14 @@
 package org.entropy;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class Main {
-    public static void main(String[] args) throws ClassNotFoundException, NoSuchFieldException, IllegalAccessException {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchFieldException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Class<?> clazz = Class.forName("org.entropy.User");
-        Method[] methods = clazz.getDeclaredMethods();
-        for (Method method : methods) {
-            System.out.println(method.getName());
-        }
+        Method method = clazz.getDeclaredMethod("myPrivateStaticMethod", String.class);
+        method.setAccessible(true);
+        method.invoke(null, "hello world");
     }
 }
