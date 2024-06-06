01ba88451497225b0499c67228306dcb0813c369
s-chance <entropy.tree@outlook.com>
2024-06-06 19:25:38 +0800

    feat(*): modify private field value

--- Reflection/src/main/java/org/entropy/Main.java
+++ Reflection/src/main/java/org/entropy/Main.java
@@ -3,11 +3,11 @@ package org.entropy;
 import java.lang.reflect.Field;
 
 public class Main {
-    public static void main(String[] args) throws ClassNotFoundException, NoSuchFieldException {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchFieldException, IllegalAccessException {
         Class<?> clazz = Class.forName("org.entropy.User");
-        Field field = clazz.getDeclaredField("comments");
-        System.out.println(field.getType());
-        System.out.println(field.getGenericType());
-        System.out.println(field.getDeclaredAnnotation(MyAnnotation.class));
+        Field field = clazz.getDeclaredField("privateStaticField");
+        field.setAccessible(true);
+        field.set(null, 100);
+        System.out.println(field.get(null));
     }
 }
