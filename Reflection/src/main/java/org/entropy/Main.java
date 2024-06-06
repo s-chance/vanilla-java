6c5800d2fd5266e117de1ad9ff4c5f3d2711241c
s-chance <entropy.tree@outlook.com>
2024-06-06 18:04:50 +0800

    feat(*): get generic type

--- Reflection/src/main/java/org/entropy/Main.java
+++ Reflection/src/main/java/org/entropy/Main.java
@@ -5,8 +5,9 @@ import java.lang.reflect.Field;
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException, NoSuchFieldException {
         Class<?> clazz = Class.forName("org.entropy.User");
-        Field field = clazz.getDeclaredField("name");
+        Field field = clazz.getDeclaredField("comments");
         System.out.println(field.getType());
+        System.out.println(field.getGenericType());
         System.out.println(field.getDeclaredAnnotation(MyAnnotation.class));
     }
 }
