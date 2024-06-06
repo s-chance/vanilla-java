c516b915391beef27caf1753045204a1320c4cfe
s-chance <entropy.tree@outlook.com>
2024-06-07 00:21:44 +0800

    perf(*): reuse instances

--- Reflection/src/main/java/org/entropy/Container.java
+++ Reflection/src/main/java/org/entropy/Container.java
@@ -8,9 +8,11 @@ import java.util.Map;
 public class Container {
     private Map<Class<?>, Method> methods;
     private Object config;
+    private Map<Class<?>, Object> services;
 
     public void init() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         this.methods = new HashMap<>();
+        this.services = new HashMap<>();
         Class<?> clazz = Class.forName("org.entropy.Config");
         Method[] methods = clazz.getDeclaredMethods();
         for (Method method : methods) {
@@ -22,10 +24,15 @@ public class Container {
     }
 
     public Object getServiceInstanceByClass(Class<?> clazz) throws InvocationTargetException, IllegalAccessException {
-        if (this.methods.containsKey(clazz)) {
-            Method method = this.methods.get(clazz);
-            Object obj = method.invoke(this.config);
-            return obj;
+        if (this.services.containsKey(clazz)) {
+            return this.services.get(clazz);
+        } else {
+            if (this.methods.containsKey(clazz)) {
+                Method method = this.methods.get(clazz);
+                Object obj = method.invoke(this.config);
+                this.services.put(clazz, obj);
+                return obj;
+            }
         }
         return null;
     }
