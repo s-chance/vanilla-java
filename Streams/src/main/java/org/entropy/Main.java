93dd005c3da10169549feb92553043283357017c
s-chance <entropy.tree@outlook.com>
2024-06-07 16:32:43 +0800

    feat(*): create stream from List

--- Streams/src/main/java/org/entropy/Main.java
+++ Streams/src/main/java/org/entropy/Main.java
@@ -3,26 +3,12 @@ package org.entropy;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        List<Person> people = List.of(
-                new Person("Peter", 33, "USA"),
-                new Person("Brain", 10, "USA"),
-                new Person("Jack", 12, "UK"),
-                new Person("Alex", 22, "USA"),
-                new Person("Steven", 24, "FR")
-        );
-        List<Person> adults = people.stream()
-                .filter(person -> person.getAge() > 18)
-                .collect(Collectors.toList());
-        System.out.println(adults);
-//        List<Person> adults = new ArrayList<>();
-//        for (Person person : people) {
-//            if (person.getAge() > 18) {
-//                adults.add(person);
-//            }
-//        }
-//        System.out.println(adults);
+        List<String> list = List.of("a", "b", "c");
+        Stream<String> stream = list.stream();
+        stream.forEach(System.out::println);
     }
 }
