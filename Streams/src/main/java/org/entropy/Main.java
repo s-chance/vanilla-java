ddcbf431fe8fcfdb800766d83ea3d753d2088496
s-chance <entropy.tree@outlook.com>
2024-06-07 23:13:36 +0800

    feat(*): intermediate operation: flatMap

--- Streams/src/main/java/org/entropy/Main.java
+++ Streams/src/main/java/org/entropy/Main.java
@@ -4,23 +4,32 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        List<Person> people = List.of(
-                new Person("Peter", 33, "USA"),
-                new Person("Brain", 10, "USA"),
-                new Person("Jack", 12, "UK"),
-                new Person("Alex", 22, "USA"),
-                new Person("Steven", 24, "FR")
+        List<List<Person>> peopleGroups = List.of(
+                List.of(
+                        new Person("Peter", 33, "USA"),
+                        new Person("Brain", 10, "USA")
+                ),
+                List.of(
+                        new Person("Jack", 12, "UK"),
+                        new Person("Alex", 22, "USA")
+                ),
+                List.of(
+                        new Person("Steven", 24, "FR")
+                )
         );
-        people.stream().map(Person::getName).forEach(System.out::println);
+        peopleGroups.stream()
+                .flatMap(Collection::stream)
+                .map(Person::getName)
+                .forEach(System.out::println);
+        peopleGroups.stream()
+                .flatMap(people -> people.stream().map(Person::getName))
+                .forEach(System.out::println);
     }
 }
