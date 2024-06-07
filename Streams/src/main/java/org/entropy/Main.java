aadd4061fd6d3320d681ebf03069b35f2dd35a2c
s-chance <entropy.tree@outlook.com>
2024-06-07 18:23:49 +0800

    feat(*): covert basic stream to object stream

--- Streams/src/main/java/org/entropy/Main.java
+++ Streams/src/main/java/org/entropy/Main.java
@@ -7,13 +7,15 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        IntStream intStream = IntStream.range(1, 4);
-        intStream.forEach(System.out::println);
+        IntStream intStream = IntStream.rangeClosed(1, 4);
+        Stream<Integer> integerStream = intStream.boxed();
+
     }
 }
