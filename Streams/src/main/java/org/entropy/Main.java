802786222ae9bc6a8264b1fd63134d35be7412a5
s-chance <entropy.tree@outlook.com>
2024-06-07 20:12:48 +0800

    feat(*): get a iterate of Stream

--- Streams/src/main/java/org/entropy/Main.java
+++ Streams/src/main/java/org/entropy/Main.java
@@ -14,6 +14,7 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        Stream.generate(Math::random).limit(5).forEach(System.out::println);
+        Stream<Integer> iterateStream = Stream.iterate(0, n -> n <= 10, n -> n + 2);
+        iterateStream.forEach(System.out::println);
     }
 }
