4fb5078202faadf71e866024b28ea4e99f95cca7
s-chance <entropy.tree@outlook.com>
2024-06-07 16:33:14 +0800

    feat(*): create stream from Array

--- Streams/src/main/java/org/entropy/Main.java
+++ Streams/src/main/java/org/entropy/Main.java
@@ -1,14 +1,15 @@
 package org.entropy;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> list = List.of("a", "b", "c");
-        Stream<String> stream = list.stream();
+        String[] array = {"a", "b", "c"};
+        Stream<String> stream = Arrays.stream(array);
         stream.forEach(System.out::println);
     }
 }
