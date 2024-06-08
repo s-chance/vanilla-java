410405087b7cce47fbf8839f60bc51b6149e9729
s-chance <entropy.tree@outlook.com>
2024-06-08 12:35:32 +0800

    feat(*): match methods in stream api

--- Streams/src/main/java/org/entropy/Main.java
+++ Streams/src/main/java/org/entropy/Main.java
@@ -11,29 +11,15 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        List<List<Person>> peopleGroups = List.of(
-                List.of(
-                        new Person("Peter", 33, "USA"),
-                        new Person("Brain", 10, "USA")
-                ),
-                List.of(
-                        new Person("Jack", 12, "UK"),
-                        new Person("Alex", 22, "USA"),
-                        new Person("Alex", 22, "USA")
-                ),
-                List.of(
-                        new Person("Steven", 24, "FR"),
-                        new Person("Steven", 24, "FR")
-                )
+        List<Person> people = List.of(
+                new Person("Peter", 33, "USA"),
+                new Person("Brain", 10, "USA"),
+                new Person("Jack", 12, "UK"),
+                new Person("Alex", 22, "USA"),
+                new Person("Steven", 24, "FR")
         );
-        Stream<String> results = peopleGroups.stream()
-                .flatMap(Collection::stream)
-                .filter(person -> person.getAge() > 18)
-                .distinct()
-                .sorted(Comparator.comparingInt(Person::getAge).reversed())
-                .map(Person::getName)
-                .limit(3)
-                .skip(1);
-        results.forEach(System.out::println);
+        boolean result = people.stream()
+                .allMatch(person -> person.getAge() != 18);
+        System.out.println(result);
     }
 }
