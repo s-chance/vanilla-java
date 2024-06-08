4a661b72eadefd71dc17c0f347901974b441320c
s-chance <entropy.tree@outlook.com>
2024-06-08 19:25:06 +0800

    feat(*): show the multi-threads work in the parallel streams

--- Streams/src/main/java/org/entropy/Main.java
+++ Streams/src/main/java/org/entropy/Main.java
@@ -12,48 +12,11 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        List<Person> people = List.of(
-                new Person("Peter", 33, "USA"),
-                new Person("Brain", 10, "USA"),
-                new Person("Jack", 12, "UK"),
-                new Person("Alex", 22, "USA"),
-                new Person("Steven", 24, "FR")
-        );
-//        ArrayList<Person> collect = people.stream().parallel()
-//                .collect(Collector.of(
-//                        () -> new ArrayList<>(),
-//                        (list, person) -> {
-//                            System.out.println("Accumulator: " + person);
-//                            list.add(person);
-//                        },
-//                        (left, right) -> {
-//                            System.out.println("Combiner: " + left);
-//                            left.addAll(right);
-//                            return left;
-//                        },
-//                        Collector.Characteristics.IDENTITY_FINISH
-//                ));
-//        System.out.println(collect);
-        int size = people.stream().parallel()
-                .collect(Collector.of(
-                        HashMap<String, List<Person>>::new,
-                        (map, person) -> {
-                            System.out.println("Accumulator: " + person);
-                            map.computeIfAbsent(person.getCountry(), k -> new ArrayList<>()).add(person);
-                        },
-                        (left, right) -> {
-                            System.out.println("Combiner: "
-                                    + System.lineSeparator() + "left: " + left
-                                    + System.lineSeparator() + "right: " + right
-                            );
-                            right.forEach((k, v) -> left.merge(k, v, (list, newList) -> {
-                                list.addAll(newList);
-                                return list;
-                            }));
-                            return left;
-                        },
-                        HashMap::size
-                ));
-        System.out.println(size);
+        List.of("E", "N", "T", "R", "O", "P", "Y").parallelStream()
+                .map(String::toLowerCase)
+                .forEach(item -> {
+                    System.out.println("Item: " + item + " -> " + " Thread: " + Thread.currentThread().getName());
+                    System.out.println(item);
+                });
     }
 }
