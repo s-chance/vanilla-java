330273bd50b496b410a163a5f1c0c38f12fd6bb6
s-chance <entropy.tree@outlook.com>
2025-04-05 17:07:26 +0800

    perf(*): use hashcode to replace iter

--- hashmap-demo/src/main/java/io/github/schance/MyHashMap.java
+++ hashmap-demo/src/main/java/io/github/schance/MyHashMap.java
@@ -5,47 +5,83 @@ import java.util.List;
 
 public class MyHashMap<K, V> {
 
-    List<Node<K, V>> table = new ArrayList<>();
+    private Node<K, V>[] table = new Node[10];
+
+    private int size = 0;
 
     public V put(K key, V value) {
-        for (Node<K, V> kvNode : table) {
-            if (kvNode.key.equals(key)) {
-                V oldValue = kvNode.value;
-                kvNode.value = value;
+        int keyIndex = indexOf(key);
+        Node<K, V> head = table[keyIndex];
+        if (head == null) {
+            table[keyIndex] = new Node<>(key, value);
+            size++;
+            return null;
+        }
+        while (true) {
+            if (head.key.equals(key)) {
+                V oldValue = head.value;
+                head.value = value;
                 return oldValue;
             }
+            if (head.next == null) {
+                head.next = new Node<>(key, value);
+                size++;
+                return null;
+            }
+            head = head.next;
         }
-        Node<K, V> newNode = new Node<>(key, value);
-        table.add(newNode);
-        return null;
     }
 
     public V get(K key) {
-        for (Node<K, V> kvNode : table) {
-            if (kvNode.key.equals(key)) {
-                return kvNode.value;
+        int keyIndex = indexOf(key);
+        Node<K, V> head = table[keyIndex];
+        while (head != null) {
+            if (head.key.equals(key)) {
+                return head.value;
             }
+            head = head.next;
         }
         return null;
     }
 
     public V remove(K key) {
-        for (int i = 0; i < table.size(); i++) {
-            Node<K, V> kvNode = table.get(i);
-            if (kvNode.key.equals(key)) {
-                Node<K, V> removedNode = this.table.remove(i);
-                return removedNode.value;
+        int keyIndex = indexOf(key);
+        Node<K, V> head = table[keyIndex];
+        if (head == null) {
+            return null;
+        }
+        if (head.key.equals(key)) {
+            table[keyIndex] = head.next;
+            size--;
+            return head.value;
+        }
+        Node<K, V> prev = head;
+        Node<K, V> current = head.next;
+        while (current != null) {
+            if (current.key.equals(key)) {
+                prev.next = current.next;
+                size--;
+                return current.value;
             }
+            prev = prev.next;
+            current = current.next;
         }
         return null;
     }
 
     public int size() {
-        return this.table.size();
+        return this.size;
     }
+
+    private int indexOf(Object key) {
+        return key.hashCode() % table.length;
+    }
+
     class Node<K, V> {
         K key;
         V value;
+        Node<K, V> prev;
+        Node<K, V> next;
 
         public Node(K key, V value) {
             this.key = key;
