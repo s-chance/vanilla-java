f4946e9284356567a705f02be766b4d4e3d32925
s-chance <entropy.tree@outlook.com>
2025-04-14 00:25:49 +0800

    feat(*): :sparkles: implements simple spring mvc framework

---
+++ spring-framework/src/main/java/io/github/schance/web/ModelAndView.java
@@ -0,0 +1,22 @@
+package io.github.schance.web;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ModelAndView {
+    private String view;
+
+    private Map<String, String> context = new HashMap<>();
+
+    public String getView() {
+        return view;
+    }
+
+    public void setView(String view) {
+        this.view = view;
+    }
+
+    public Map<String, String> getContext() {
+        return context;
+    }
+}
---
+++ spring-framework/src/main/java/io/github/schance/web/WebHandler.java
@@ -0,0 +1,44 @@
+package io.github.schance.web;
+
+import java.lang.reflect.Method;
+
+public class WebHandler {
+
+    private final Object controllerBean;
+
+    private final Method method;
+
+    private final ResultType resultType;
+
+    public WebHandler(Object controllerBean, Method method) {
+        this.controllerBean = controllerBean;
+        this.method = method;
+        this.resultType = resolveResultType(controllerBean, method);
+    }
+
+    private ResultType resolveResultType(Object controllerBean, Method method) {
+        if (method.isAnnotationPresent(ResponseBody.class)) {
+            return ResultType.JSON;
+        }
+        if (method.getReturnType() == ModelAndView.class) {
+            return ResultType.LOCAL;
+        }
+        return ResultType.HTML;
+    }
+
+    public Object getControllerBean() {
+        return controllerBean;
+    }
+
+    public Method getMethod() {
+        return method;
+    }
+
+    public ResultType getResultType() {
+        return resultType;
+    }
+
+    enum ResultType {
+        JSON, HTML, LOCAL
+    }
+}
